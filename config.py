2170c160ce3b8a59ee4d5ebbecf2f744e930d414
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2026-03-01 17:54:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,12 @@ class Config:
 
     # 🔥 EMAIL CONFIG (GMAIL SMTP)
 
-    MAIL_SERVER = "smtp.gmail.com"
+     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-
-    MAIL_USERNAME = "mraviteja.2807@gmail.com"   # 🔴 CHANGE
-    MAIL_PASSWORD = "wtug hqgr imbj okce"      # 🔴 CHANGE
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
 
     MAIL_DEFAULT_SENDER = "mraviteja.2807@gmail.com" 
